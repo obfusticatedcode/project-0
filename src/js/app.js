cddfb5d2ef34f47aca60f5d5edd57fdb3b88d9f6
Chisomo Lungu <chisomo@gmail.com>
2017-05-22 21:20:51 +0100

    new ideas

--- src/js/app.js
+++ src/js/app.js
@@ -1,243 +1,239 @@
-// load jQuery
+//loading JS
 $(()=>{
-// TEST jQuery is working
-  console.log(`jQuery is working!`);//passed. output is jQuery is working!
 
+  //GLOBAL VARIABLES
+  const width = 46;
+  const border = 2;
+  const $board = $('div#board');
+  const $pieces = $('div#pieces');
 
-//grab DOM elements
-  const $checkersBoard = $('#checkers-board');
-  const $squaresOnBoard = $('.squares');
-  const $pieces = $('.pieces');
-  //player1 piece class
-  const $player1 =$('.player1');
-  //player2 piece class
-  const $player2 =$('.player2');
-  let $player1Pieces = undefined;//not yet generated
-  //player2 pieces
-  let $player2Pieces = undefined;//not yet generated
-  //the selected class
-  const $selected =$('#checkers-board .pieces .player1 .selected');//is this a dom element?
+  //utility function for translating an x,y coordinate
+  //to a pixel position
+  //the convention is that the square in the upper left
+  //corner is at position 0,0
+  //the square in the upper right, at 7,0 and the lower
+  //right at 7,7
+  function getPixels(x,y) {
+      //ok... so takes an x,y position, returns
+      //pixels from the left, right
 
-
-//TEST dom elements are working
-  console.log(`${$checkersBoard} `);//output is object Object
-
-
-//setup GLOBAL VARIABLES
-//board array with rows and columns to simulate the real world 8x8 board
-  const checkersBoard = [
-    [  0,  1,  0,  1,  0,  1,  0,  1 ],
-    [  1,  0,  1,  0,  1,  0,  1,  0 ],
-    [  0,  1,  0,  1,  0,  1,  0,  1 ],
-    [  0,  0,  0,  0,  0,  0,  0,  0 ],
-    [  0,  0,  0,  0,  0,  0,  0,  0 ],
-    [  2,  0,  2,  0,  2,  0,  2,  0 ],
-    [  0,  2,  0,  2,  0,  2,  0,  2 ],
-    [  2,  0,  2,  0,  2,  0,  2,  0 ]
-  ];
-
-//incremental arrays
-  const pieces = [];
-  const squares = [];
-//players
-  const player1 = 1;
-  const player2 = 2;
-//players turn
-  let playerTurn = null;
-//counters
-  let countSquares = 0;
-  let countPieces = 0;
-//array with view port conversion with top and left
-//Viewport Minimum (vmin) — This unit is based on
-//the smaller dimension of the viewport.
-//If the viewport height is smaller than the width,
-//the value of 1vmin will be equal to 1% of the viewport height.
-//Similarly, if the viewport width is smaller than the height, the value of 1vmin will be equal to 1% of the viewport width.
-  const viewportConversion = ['0vmin','10vmin','20vmin','30vmin','40vmin','50vmin','60vmin','70vmin'];
-
-  //call the SETUP function/method to create the board and add pieces
-  setup();
-
-  function setup(){
-    //player 1 always starts
-    playerTurn = player1;
-    //loop through the array called checkersBoard indexed at 0
-    for (let row = 0; row < checkersBoard.length; row++) {
-      checkersBoard[row];
-        //TEST the rows index is working
-        // console.log(row);//passed. output is 0,1,2,3,4,6,7
-        //loop through the array elements or columns
-
-      for (let column = 0; column < checkersBoard[row].length; column++) {
-        checkersBoard[row][column];
-        //TEST the column index is being looped through
-          // console.log(column);//passed. output is 0,1,2,3,4,6,7 foreach column
-
-        //if the row is odd and the column even add a square
-        if(row % 2 === 1 && column % 2 === 0 ){
-            //TEST the row is correct-odd
-            // console.log(row);//pass. output is 1,3,5,7
-              //TEST the column is correct-even
-              // console.log(column);//passed. output is 0,2,4,6
-              //append a div representing a square to the $squaresOnBoard div with class
-          $squaresOnBoard.append(`<div class='square' id='square${countSquares}' style='top:${viewportConversion[row]}; left:${viewportConversion[column]}'></div>`);
-              //create a new squareObject and add the number of squares count to the squares counters
-              //use the element id and position as arguments to create the squares
-          squares[countSquares] = new square($('#squares'+countSquares), [parseInt(row), parseInt(column)]);
-          countSquares++;
-              // //TEST the countSquares works
-              // console.log(countSquares);//passed. output is 1 to 16 index based is 0 to 15
-        }else if(row % 2 === 0 && column % 2 === 1) {//otherwise if row is even.
-            //TEST the column is correct-odd
-            // console.log(column);//passed. output is 1,3,5,7
-          $squaresOnBoard.append(`<div class='square' id='square${countSquares}' style='top:${viewportConversion[row]}; left:${viewportConversion[column]}'></div>`);
-          squares[countSquares] = new square($('#square'+countSquares), [parseInt(row), parseInt(column)]);
-              // console.log(`id='square${countSquares}`);
-          countSquares++;
-
-        }//end of else statement
-
-            //add the pieces to the checkersBoard
-        if(checkersBoard[row][column] === player1) {
-          $player1.append(`<div class='player1-pieces' id='${countPieces}' style='top:${viewportConversion[row]}; left:${viewportConversion[column]};'></div>`);
-          pieces[countPieces] = new piece($('#'+countPieces), [parseInt(row), parseInt(column)]);
-          countPieces++;
-            //TEST value of checkersBoard[row][column]
-          // console.log(`checkersBoard row is: ${checkersBoard[row]} `);
-          // console.log(`checkersBoard column is: ${checkersBoard[column]} `);
-          // console.log(`checkersBoard row and column is: ${checkersBoard[row][column]}`);//passed.output is 1
-        }else if(checkersBoard[row][column] === player2) {
-          $player2.append(`<div class='player2-pieces' id='${countPieces}' style='top:${viewportConversion[row]}; left:${viewportConversion[column]};'></div>`);
-          pieces[countPieces] = new piece($('#'+countPieces), [parseInt(row), parseInt(column)]);
-          countPieces++;
-            //TEST value of checkersBoard[row][column]
-          // console.log(`checkersBoard row is: ${checkersBoard[row]} `);
-          // console.log(`checkersBoard column is: ${checkersBoard[column]} `);
-          // console.log(`checkersBoard 2 row and column is: ${checkersBoard[row][column]}`);//passed. output is 2
-        }
-
-      }//end of inner for loop
-    }//end of outter for loop
-  }//end of setup function
-
-  //player1 pieces grabbing these here after they've been generated declare at the top.
-  $player1Pieces =$('.player1-pieces');
-  //player2 pieces
-  $player2Pieces =$('.player2-pieces');
-
-  //SQUARE is empty
-  function squareIsEmpty(row, column){
-    //TEST row and column
-    this.row = row;
-    this.column = column;
-    console.log(`value of row:${row} and value of column: ${column}`);
-    //using a tenary operator (an operator accepting 3 operands)as shorthand
-    //to if else check if there's an object
-    //on the row and column (square)
-    checkersBoard[row][column] === 0 ? true : false;
-  }
-
-
-
-//SQUARE
- //create a square object to attach to the checkersBoard
-  function square (element, position) {
-    //using this to give local variables a context
-    //element is the the div linked to the DOM
-    this.element = element;
-    // the positions are represented by the multidimensional array as row and column format
-    this.position = position;
-    //TEST the square position
-    // console.log(`This squares' position is: ${position}`);
-    //if square is in range from the piece
-    this.inRange = function(piece) {
-      if(distance(this.position[0], this.position[1], piece.position[0],
-                  piece.position[1]) === Math.sqrt(2)) {
-      //normal-move
-        return 'normal-move';
-      } else if(distance(this.position[0], this.position[1],
-                  piece.position[0], piece.position[1]) === 2 * Math.sqrt(2)) {
-      //jump-move
-        return 'jump-move';
-      }
+    return {
+      'top': (y * (width+border))+'px',
+      'left': (x * (width+border))+'px'
     };
   }
 
-  //DISTANCE
-  // We use the Pythagoras Theorem to derive a formula for
-  // finding the distance between two points in 2- and 3- dimensional space.
-  function distance(x1,x2,y1,y2){
-    return Math.sqrt(Math.pow((x1-x2),2)+ Math.pow((y1-y2),2));
+  //utility function for turning a pixel position
+  //into the x,y coordinate of a square on the board
+  //it follows the same coordinate convention as getPixels
+  function getCoords(top,left) {
+      //returns an x and a y
+      //given a top and left pixels
+    return {
+      'x': left / (width + border),
+      'y': top / (width + border)
+    };
   }
 
-  //PIECES
-  function piece (element, position) {
-    //using this to give local vars a context
-    //element is the the div linked to the DOM
-    this.element = element;
-    // console.log(this.element);
-    //positions on gameBoard array in format row, column
-    this.position = position;
-    //TEST the position
-    // console.log(`The piece position is: ${position}`);
+  //utility function for returning
+  //the set of unoccupied dark squares
+  //(possible places to move a checker piece)
+  function getMovableSquares() {
 
-    //who's piece is it, player place holder
-    this.player = '';
-    //figure out player by piece id
-    if(this.element < 12)
-      this.player1;
-    else
-      this.player2;
+      //select all of the squares
+    const $squares = $('div.square');
+
+      //select the occupied ones using the jQuery map() method
+      //map creates a new object from an existing one
+      //using a translation function
+    const $takenSquares =
+          $('div.piece').map(function(index,piece) {
+
+              //this function translates a piece
+            const position = $(piece).position();
+            const coords = getCoords(position.top,position.left);
+            const squareIndex = coords.y * 8 + coords.x;
+            return $squares[squareIndex];
+          });
+
+    const $out = $('div.square.dark').not($takenSquares);
+    return $out;
   }
 
-  // CLICK EVENTS
-  $squaresOnBoard.on('click',()=>{
-    //TEST the click event works.
-    console.log(`click events works on the squares!`);
+  $('document').ready(function() {
+
+      //Creating the 64 squares and adding them to the DOM
+    const squareCount = 8*8;
+    for (let i = 0;i<squareCount;i++) {
+
+          //this line creates a new div with the class 'square'
+          //and appends it to the div with id 'board'
+      $board.append($('<div/>').addClass('square'));
+    }
+
+      //YOUR CODE
+      //set up the board with the correct classes
+      //for the light and dark squares
+    setUpBoard();
+
+
+      //creating the 24 pieces and adding them to the DOM
+    const pieceCount = 24;
+    for (let i=0;i<pieceCount;i++) {
+
+          //this line appends an empty div
+          //with the class 'piece' to the div with id 'pieces'
+      $pieces.append($('<div/>').addClass('piece'));
+
+    }
+
+      //YOUR CODE
+      //sets up the classes for the different types of piece
+    setUpPieces();
+
+      //this loop moves all the light pieces to their initial positions
+    $('div.piece.light').each(function(index,piece) {
+
+          //turning the index (from 0 - 11)
+          //into a x,y square coordinate using math
+      const y = Math.floor(index / 4);
+      const x = (index % 4) * 2 + (1 - y%2);
+
+          //turning the x,y coordingate into a pixel position
+      const pixelPosition = getPixels(x,y);
+
+          //YOUR CODE
+          //actually moving the piece to its initial position
+      movePieceTo($(piece),pixelPosition.top,pixelPosition.left);
+    });
+
+      //this loop moves all the dark pieces to their initial positions
+    $('div.piece.dark').each(function(index,piece) {
+
+          //turning the index (from 0 - 11)
+          //into a x,y square coordinate using math
+      const y = Math.floor(index/4) + 5;
+      const x = (index % 4) * 2 + (1-y%2);
+
+          //turning the x,y coordinate into a pixel position
+      const pixelPosition = getPixels(x,y);
+
+          //YOUR CODE
+          //moving the piece to its initial position
+      movePieceTo($(piece),pixelPosition.top,pixelPosition.left);
+    });
+
+      //set up initial squares
+      //the class 'movable' represents a square
+      //that is unoccupied
+    getMovableSquares().addClass('movable');
+
+      //and now the events
+    $('div.piece').click(function() {
+          //turn `this` into a jQuery object
+      const $this = $(this);
+
+          //YOUR CODE
+          //toggleing the 'selected' class of this piece
+          //and possible deselecting other pieces
+      toggleSelect($this);
+    });
+
+    $('div.square').click(function() {
+
+          //turn `this` into a jQuery object
+      const $this = $(this);
+
+          //if $this is a legal square to move to...
+      if ($this.hasClass('movable')) {
+
+              //get the piece with the class 'selected'
+        const $selectedPiece = $('div.piece.selected');
+
+              //we only move if there is exactly one selected piece
+        if ($selectedPiece.length === 1) {
+                  //get the index of the square
+                  //and translate it to pixel position
+          const index = $this.prevAll().length;
+          const x = index % 8;
+          const y = Math.floor(index / 8);
+          const pixels = getPixels(x,y);
+
+                  //YOUR CODE
+                  //actually do the moving
+          movePieceTo($selectedPiece,pixels.top,pixels.left);
+
+                  //YOUR CODE
+                  //increment the move counter
+          incrementMoveCount();
+
+                  //un-select the piece
+          $selectedPiece.removeClass('selected');
+
+                  //set the new legal moves
+          $('div.square').removeClass('movable');
+          getMovableSquares().addClass('movable');
+        }
+
+      }
+    });
+
   });
 
-  //SELECTING PIECES
-  $pieces.on('click',()=>{
-    //TEST the click event works.
-    console.log(`click events works on the pieces!`);
-    // $player2Pieces.toggleClass('selected');
-    //TEST to see the id on the pieces, will use this to select each one.
-    // console.log($player1Pieces);
-    console.log($(this).attr('class'));//pieces div has no id therefore returns undefined
-    const classn = $(this).parent().attr('class');
-    console.log(`This is a parent class name: ${classn}`);
-    // console.log(`This is a class name: ${classn}`);
+  // FUNCTIONS
+  function setUpPieces() {
+      //select all the divs with class 'piece'
+      //add the 'light' class to half of them
+      //add the 'dark' to the other half
 
-  });
+  }
 
-  // figuring out how to grab single piece
-  //TEST for grabbing id's
-  $player1.on('click',()=>{
-    console.log(`This is player 1 div and it's been clicked`);
+  function movePieceTo($piece,newTop,newLeft) {
+      //set the css 'top' and 'left'
+      //attributes of the passed piece
+      //to the arguments newTop and newLeft
 
-    console.log($(this).attr('id'));//id of the current div..output is undefined
-    // const id = event.target.id;
-    // console.log(id);
+  }
 
-  });
+  function setUpBoard() {
+      //iterate through all of the divs
+      //with class `square`
+      //figure out whether each one should be
+      //light or dark, and assign the proper class
+
+      //heres a helper function that takes a number between
+      //0 and 63 (inclusive) and returns 1 if the square should be
+      //dark, and 0 if the square should be light
+    function lightOrDark(index) {
+      const x = index % 8;
+      const y = Math.floor(index / 8);
+      const oddX = x % 2;
+      const oddY = y % 2;
+      return (oddX ^ oddY);
+    }
 
 
 
-  $player1Pieces.on('click', (e)=> {
-    $player1Pieces.removeClass('selected');
-    $(e.target).addClass('selected');
-    const $test = $('.player1');
-    $test.toggleClass('selected');
-    console.log(`Output of the test : ${$test}`);
+  }
 
-    // $player1Pieces.toggleClass('selected');
-    // const id = $(event.target);//using event.target  to get the child id.
-    // console.log(id.attr('id'));//passed. outputs the div id of the selected piece
-    // console.log($(this).attr('id'));//$player1Pieces div has no id therefore returns undefined
-  });
+  function toggleSelect($piece) {
+      //if $piece has the class 'selected',
+      //remove it
 
-  $player2Pieces.on('click', ()=>{
-    $player2Pieces.toggleClass('selected');
-  });
+      //if $piece does not have the class 'selected'
+      //make sure no other divs with the class 'piece'
+      //have that class, then set $piece to have the class
 
-});//end of jQuery load
+  }
+
+  function incrementMoveCount() {
+      //gets the html of the span with id
+      //moveCount
+      //turns it into a number
+      //increments it by one
+      //sets the html of the span with id moveCount
+      //to the new move count
+  }
+
+
+});//end of JS load
