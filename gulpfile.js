3777485e58fc6b8cc53172473130125150a4a2e5
Chisomo Lungu <chisomo@gmail.com>
2025-01-26 18:40:17 +0000

    fix(build): resolve gulp-uglify compatibility issues with modern JavaScript
    - Replaced `gulp-uglify` with `gulp-terser` to support modern JavaScript syntax.
    - Updated the `es6` task in `gulpfile.js` to use `gulp-terser` for minification.
    - Removed `gulp-uglify` and added `gulp-terser` as a development dependency.
    - Verified compatibility with Babel-transpiled ES6+ code.
    
    BREAKING CHANGE: Projects relying on `gulp-uglify` are now required to use `gulp-terser` for JavaScript minification. Ensure your project setup supports this transition.

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,89 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
-const notify = require('gulp-notify');
-const plumber = require('gulp-plumber');
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const sass = require("gulp-sass")(require("sass")); // Dart Sass
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const browserSync = require("browser-sync").create();
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
 
+/**
+ * Reports errors during task execution and prevents Gulp from crashing.
+ * @param {Error} error - The error object thrown during task execution.
+ * @this {Stream} - The current Gulp stream, to allow proper chaining.
+ */
 function reportError(error) {
-  notify({
-    title: `Task Failed [${error.plugin}]`,
-    message: 'Check the console.'
-  }).write(error);
-  console.log(error.toString());
-  this.emit('end');
+  notify.onError({
+    title: `Task Failed: [${error.plugin}]`,
+    message: "Check your console.",
+    sound: false,
+  })(error);
+  console.error(error.toString());
+  this.emit("end");
 }
 
+/**
+ * Compiles SCSS files into minified CSS with sourcemaps.
+ * @returns {Stream} - Gulp stream processing SCSS files.
+ */
 function css() {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp
+    .src("src/scss/**/*.scss", { sourcemaps: true })
     .pipe(plumber({ errorHandler: reportError }))
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/css'));
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("public/css", { sourcemaps: "." }));
 }
 
+/**
+ * Transpiles JavaScript (ES6+) to ES5, minifies, and generates sourcemaps.
+ * @returns {Stream} - Gulp stream processing JavaScript files.
+ */
 function es6() {
-  return gulp.src('src/js/*.js')
+  return gulp
+    .src("src/js/*.js", { sourcemaps: true })
     .pipe(plumber({ errorHandler: reportError }))
-    .pipe(sourcemaps.init())
-    .pipe(babel({ presets: [["@babel/preset-env", { "targets": "defaults" }], "@babel/preset-react"] }))
+    .pipe(
+      babel({
+        presets: [
+          ["@babel/preset-env", { targets: "defaults" }],
+          "@babel/preset-react",
+        ],
+      })
+    )
     .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/js'));
+    .pipe(gulp.dest("public/js", { sourcemaps: "." }));
 }
 
+/**
+ * Copies static assets (images, fonts, etc.) to the `public/assets` directory.
+ * @returns {Stream} - Gulp stream for static assets.
+ */
 function assets() {
-  return gulp.src('src/assets/**/*')
-    .pipe(gulp.dest('public/assets'));
+  return gulp.src("src/assets/**/*").pipe(gulp.dest("public/assets"));
 }
 
+/**
+ * Initializes a local development server with live reload.
+ * @returns {void}
+ */
 function serve() {
-  gulp.series(es6, css)
   browserSync.init({
-    files: ['public/**/*.*'],
-    browser: 'google chrome',
+    files: ["public/**/*.*"],
+    browser: "google chrome",
     port: 7000,
     reloadDelay: 500,
-    server: { baseDir: './' }
+    server: { baseDir: "./" },
   });
+
+  gulp.watch("src/scss/**/*.scss", css).on("change", browserSync.reload);
+  gulp.watch("src/js/*.js", es6).on("change", browserSync.reload);
+  gulp.watch("src/assets/**/*", assets).on("change", browserSync.reload);
 }
 
-function watch() {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/js/*.js', ['es6']);
-  gulp.watch('src/assets/**/*', ['assets']);
-}
-
-exports.default = gulp.series(css, es6, assets, serve, watch)
+/**
+ * Default Gulp task: builds CSS, JS, and assets, then starts the dev server.
+ * @returns {void}
+ */
+exports.default = gulp.series(gulp.parallel(css, es6, assets), serve);
